d4b4ef44b7205c8b77a183eaf22f8067613648af
desouzapreston <desouzapreston@gmail.com>
2019-10-31 08:07:22 -0500

    Updates from 10/30: Abstraction, python web server

--- reader.py
+++ reader.py
@@ -1,15 +1,9 @@
 import csv
 from scraper import get_product_details
 from url_response import get_custom_url
-import sqlite3
 
-#db
-conn = sqlite3.connect('new_egg.db')
-cur = conn.cursor()
-# cur.execute('CREATE TABLE scrapings (title VARCHAR, price VARCHAR, image VARCHAR)')
-# conn.commit()
 
-def read_csv(conn, cur):
+def read_csv():
     with open('ScrapperSheet2.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         man_part_nums = []
@@ -18,15 +12,5 @@ def read_csv(conn, cur):
         set_part_nums = set(man_part_nums)
         i_s_part_nums = iter(set_part_nums)
         print("\n")
-        for part_num in i_s_part_nums:
-            product_url = get_custom_url(part_num)
-            print(product_url)
-            product_info = get_product_details(product_url, part_num, cur, conn)
-            print(product_info)
-            print("\n")
-            
-# function call
-read_csv(conn, cur)
+        return i_s_part_nums
 
-#DB End Connection        
-conn.close()
