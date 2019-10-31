9a34f822d13ebc9eb8c25a9a2d893eeec9158eb5
desouzapreston <desouzapreston@gmail.com>
2019-10-31 14:58:54 -0500

    Updated landing_page.html & LP.py

--- templates/landing_page.py
+++ templates/landing_page.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template 
-app = Flask(__name__) @app.route('/') 
+from flask import (
+    Flask,
+    render_template
+)
 
+# application instance
+app = Flask(__name__, template_folder="templates") 
+# Read the swagger.yml file to configure the endpoints
+app.add_api('swagger.yml')
+
+@app.route('/')
 def home(): 
-    return render_template("landing_page.html") 
+    return render_template('landing_page.html') 
 
 if __name__ =='__main__': 
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
