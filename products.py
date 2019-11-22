c176d9d920d9a2305f80390aa7393aa695bca62c
desouzapreston <desouzapreston@gmail.com>
2019-11-22 08:36:41 -0600

    Updating Swagger.yml

--- products.py
+++ products.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 
+from flask import make_response, abort
+
 def get_timestamp():
     return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
 
-# Data to serve with our API
+# TODO: Data needs to be passed in via dict valuekey pair form
 PRODUCTS = {
     "Farrell": {
         "fname": "Doug",
@@ -22,13 +24,68 @@ PRODUCTS = {
     }
 }
 
-# Create a handler for our read (GET) products
-def read():
-    """
-    This function responds to a request for /api/products
-    with the complete lists of products
-
-    :return:        sorted list of products
-    """
-    # Create the list of products from our data
+def read_all():
+    # json string of all products available
+    # read in from reader (dict key:value)
     return [PRODUCTS[key] for key in sorted(PRODUCTS.keys())]
+
+
+def read_one(part_num):
+    # Individual product selector
+    if part_num in PRODUCTS:
+        product = PRODUCTS.get(part_num)
+    else:
+        abort(
+            404, "{part_num} not found. Please try again".format(part_num=part_num)
+        ) #loop through to reentry
+
+    return product
+
+"""def delete(part_num):
+    # Deletes product from product list PRODUCTS
+    if part_num in PRODUCTS:
+        del PRODUCTS[part_num]
+        return make_response(
+            "{part_num} successfully deleted".format(part_num=part_num), 200
+        )
+    else:
+        abort(
+            404, "product with last name {part_num} not found".format(part_num=part_num)
+        )
+"""
+
+"""def create(product):
+    # Create a new product
+    part_num = product.get("part_num", None)
+    fname = product.get("fname", None)
+
+    # Does the product exist already?
+    if part_num not in PRODUCTS and part_num is not None:
+        PRODUCTS[part_num] = {
+            "part_num": part_num,
+            "fname": fname,
+            "timestamp": get_timestamp(),
+        }
+        return make_response(
+            "{part_num} successfully created".format(part_num=part_num), 201 # Success
+        )
+
+    # Otherwise, they exist, that's an error
+    else:
+        abort(
+            406, # Product Exists
+            "product with last name {part_num} already exists".format(part_num=part_num),
+        )
+"""
+
+"""def update(part_num, product):
+    # Updates existing product in product list PRODUCTS 
+    if part_num in PRODUCTS:
+        PRODUCTS[part_num]["fname"] = product.get("fname")
+        PRODUCTS[part_num]["timestamp"] = get_timestamp()
+        return PRODUCTS[part_num]
+    else:
+        abort(
+            404, "product with last name {part_num} not found".format(part_num=part_num)
+        )
+"""
