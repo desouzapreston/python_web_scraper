ee4674c473144ff6edcf4b6fa83cdba2b49e73f6
desouzapreston <desouzapreston@gmail.com>
2019-12-16 10:17:07 -0600

    prt1 Generating filters

--- landing_page.js
+++ landing_page.js
@@ -1,12 +1,19 @@
-
-
 /* Get all elements with class="close" */
 var closebtns = document.getElementsByClassName("close");
 var i;
-
 /* Loop through the elements, and hide the parent, when clicked on */
 for (i = 0; i < closebtns.length; i++) {
     closebtns[i].addEventListener("click", function() {
         this.parentElement.style.display = 'none';
     });
+}
+
+/* Get all elements with class="generate" */
+var generatebtns = document.getElementsByClassName("generate");
+var j;
+/* Loop, show new filters, when clicked on */
+for (i = 0; i < generatebtns; i++) {
+    generatebtns[i].addEventListener("click", function() {
+        //TODO: use jQuery Append to generate new filter tag
+    })
 }
