68aa86c9d71bbe166a225abf15924e429ad64532
desouzapreston <desouzapreston@gmail.com>
2019-12-17 08:21:28 -0600

    Trying to fix the PHP download button bug

--- landing_page.php
+++ landing_page.php
@@ -1,3 +1,3 @@
 <?php
-exec("python landing_page.py ".$product_number, $results_amount, $min_price, $max_price, $output);
+exec("python landing_page.py ".$prdnumber, $prdresults, $prdmin, $prdmax, $output);
 ?>
